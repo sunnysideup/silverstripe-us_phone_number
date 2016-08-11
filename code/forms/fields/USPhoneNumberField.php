b31656d73a4d0d60d2671a89a729a38d3ba32d0d
Nicolaas <github@sunnysideup.co.nz>
2016-08-11 22:11:46 +1200

    bugfix

--- code/forms/fields/USPhoneNumberField.php
+++ code/forms/fields/USPhoneNumberField.php
@@ -2,20 +2,20 @@
 
 class USPhoneNumberField extends TextField {
 
-	/**
-	 * Includes the JavaScript neccesary for this field to work using the {@link Requirements} system.
-	 */
-	public static function include_js() {
-		Requirements::javascript(FRAMEWORK_DIR.'/thirdparty/jquery/jquery.js');
-		Requirements::javascript(FRAMEWORK_DIR.'/thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
-		Requirements::javascript('usphonenumber/javascript/USPhoneNumber.js');
-		Requirements::themedCSS('USPhoneNumber', 'usphonenumber');
-	}
+    /**
+     * Includes the JavaScript neccesary for this field to work using the {@link Requirements} system.
+     */
+    public static function include_js() {
+        Requirements::javascript(FRAMEWORK_DIR.'/thirdparty/jquery/jquery.js');
+        Requirements::javascript(FRAMEWORK_DIR.'/thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
+        Requirements::javascript('us_phone_number/javascript/USPhoneNumber.js');
+        Requirements::themedCSS('USPhoneNumber', 'us_phone_number');
+    }
 
-	public function Field($properties = array()) {
-		$this->addExtraClass('text');
-		$return = parent::Field($properties = array());
-		self::include_js();
-		return $return;
-	}
+    public function Field($properties = array()) {
+        $this->addExtraClass('text');
+        $return = parent::Field($properties = array());
+        self::include_js();
+        return $return;
+    }
 }
