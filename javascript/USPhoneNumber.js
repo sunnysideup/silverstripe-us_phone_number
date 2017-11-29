61c686fc160ba125b8b12e8d44eab6d400fdb726
ShaneW <sunnysideupteam@gmail.com>
2017-11-29 16:00:09 +1300

    MINOR: rewriting JS to work with jQuery rather than entwine.

--- javascript/USPhoneNumber.js
+++ javascript/USPhoneNumber.js
@@ -1,75 +1,78 @@
 
-
-(function($) {
-    $.entwine(
-        'ss.usphonenumber',
-        function($){
-            $('input.usphonenumber').entwine(
-                {
-                    formatPhoneNumber: function() {
-                        var el = $(this);
-                        if(jQuery(el).is(':visible')){
-                            var val = jQuery(el).val();
-                            val = val.replace(/\D/g,'');
-                            // do validation tests
-                            if (this.validateNumber()) {
-                                // (123) 335-6789
-                                val = val.replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
-                                jQuery(el).val(val);
-                            }
-                            else if(val.length > 0) {
-                                jQuery(el).focus();
-                            }
-                        }
-                    },
-
-                    /**
-                     * adds class and return true (valid)
-                     * or false (invalid)
-                     * @param el
-                     * @return boolean
-                     *
-                     */
-                    validateNumber: function(){
-                        var el = $(this);
-                        var val = jQuery(el).val();
-                        val = val.replace(/\D/g,'');
-                        // do validation tests
-                        if (val.length == 10 || val.length == 0) {
-                            jQuery(el).removeClass("usphonenumbererror");
-                            jQuery(el).addClass("usphonenumberok");
-                            return true;
-                        }
-                        else {
-                            jQuery(el).addClass("usphonenumbererror");
-                            jQuery(el).removeClass("usphonenumberok");
-                            return false;
-                        }
-                    },
-
-                    onadd: function() {
-                        this.formatPhoneNumber();
-                    },
-                    onpropertychange: function() {
-                        this.validateNumber();
-                    },
-                    onchange: function() {
-                        this.formatPhoneNumber();
-                    },
-                    onclick: function() {
-                        this.validateNumber();
-                    },
-                    onkeyup: function() {
-                        this.validateNumber();
-                    },
-                    oninput: function() {
-                        this.validateNumber();
-                    },
-                    onpaste: function() {
-                        this.validateNumber();
-                    }
-                }
-            );
+if(jQuery('.usphonenumber').length) {
+    jQuery(document).ready(
+        function(){
+            USPhoneField.init();
+            USPhoneField.activateFieldListeners();
         }
     );
-}(jQuery));
+}
+
+var USPhoneField = {
+
+    init: function() {
+        jQuery('input.usphonenumber').each(
+            function(i, el){
+                var el = jQuery(this);
+                USPhoneField.formatPhoneNumber(el);
+            }
+        );
+    },
+
+    activateFieldListeners: function(){
+        jQuery('input.usphonenumber').on(
+            'add, change',
+            function(){
+                var el = jQuery(this);
+                USPhoneField.formatPhoneNumber(el);
+            }
+        );
+
+        jQuery('input.usphonenumber').on(
+            'propertchange, click, keyup, input, paste',
+            function(){
+                var el = jQuery(this);
+                USPhoneField.formatPhoneNumber(el);
+            }
+        );
+    },
+
+    formatPhoneNumber: function(el) {
+        if(el.is(':visible')){
+            var val = jQuery(el).val();
+            val = val.replace(/\D/g,'');
+            // do validation tests
+            if (USPhoneField.validateNumber(el)) {
+                // (123) 335-6789
+                val = val.replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+                el.val(val);
+            }
+            else if(val.length > 0) {
+                el.focus();
+            }
+        }
+    },
+
+    /**
+     * adds class and return true (valid)
+     * or false (invalid)
+     * @param el
+     * @return boolean
+     *
+     */
+    validateNumber: function(el){
+        var val = el.val();
+        val = val.replace(/\D/g,'');
+        // do validation tests
+        if (val.length == 10 || val.length == 0) {
+            el.removeClass("usphonenumbererror");
+            el.addClass("usphonenumberok");
+            return true;
+        }
+        else {
+            el.addClass("usphonenumbererror");
+            el.removeClass("usphonenumberok");
+            return false;
+        }
+    }
+}
